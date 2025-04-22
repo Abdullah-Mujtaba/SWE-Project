59d2e8277ec41882b441d81501b936a219183d76
A. Abdullah Mujtaba <122427882+Abdullah-Mujtaba@users.noreply.github.com>
2025-04-22 13:19:20 +0500

    added 11tables from the database and the whole database file

---
+++ Webapp/dbfiles/11tables.sql
@@ -0,0 +1,12 @@
+SELECT * FROM [User]
+SELECT * FROM Carts
+SELECT * FROM Products
+SELECT * FROM Category
+SELECT * FROM SubCategory
+SELECT * FROM CartItems
+SELECT * FROM coupons
+SELECT * FROM discount_offers
+SELECT * FROM product_images
+SELECT * FROM orders
+SELECT * FROM reviews
+
